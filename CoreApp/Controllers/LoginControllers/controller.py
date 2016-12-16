568f2682ae5b5eae20c70d4e3d1c315fe07ba763
Kiran Karpurapu <kvk229@nyu.edu>
2016-12-16 01:56:58 -0500

    added the templates folder

--- CoreApp/Controllers/LoginControllers/controller.py
+++ CoreApp/Controllers/LoginControllers/controller.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
-from django.http import Http404
+
 
 def render_result(request, question_id):
-	marks = 2 * int(question_id)
-	# template = loader.get_template('eatOutApp/index.html')
-	context = {
-		'marks' : marks,
-	}
-	# raise Http404("Question does not exist")
-	return render(request, 'eatOutApp/index.html', context)
-
+    marks = 2 * int(question_id)
+    # template = loader.get_template('eatOutApp/index.html')
+    context = {
+        'marks': marks,
+    }
+    # raise Http404("Question does not exist")
+    return render(request, 'CoreApp/index.html', context)
